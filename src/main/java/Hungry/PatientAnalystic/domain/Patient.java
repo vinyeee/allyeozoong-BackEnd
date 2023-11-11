9c38cdb5c63804619cc15bf1a25d8223242526ce
yooonwodyd <dbswodyd00@naver.com>
2023-11-12 01:20:42 +0900

    OneToOne에 있어서 변경된 설계 적용

--- src/main/java/Hungry/PatientAnalystic/domain/Patient.java
+++ src/main/java/Hungry/PatientAnalystic/domain/Patient.java
@@ -13,11 +13,22 @@ public class Patient {
     @Column(name = "patient_id", nullable = false)
     private Long id;
 
-    private String name; // 이름
-
-    private int age; // 나이
+    @OneToOne
+    @JoinColumn(name = "user_account_user_id")
+    UserAccount userAccount;
 
     private String symptom; // 증상
 
     private int lisk;
+
+    public Patient(Long id, UserAccount userAccount, String symptom, int lisk) {
+        this.id = id;
+        this.userAccount = userAccount;
+        this.symptom = symptom;
+        this.lisk = lisk;
+    }
+
+    public Patient() {
+
+    }
 }
