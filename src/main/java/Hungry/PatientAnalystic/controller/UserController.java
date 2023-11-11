a4ef5570bd6fb1f9082f16422468bc8f32cd0ba7
yooonwodyd <dbswodyd00@naver.com>
2023-11-12 01:23:35 +0900

    유저 APi 및 유저 로그인을 위해 Login Controller 만들기

--- src/main/java/Hungry/PatientAnalystic/controller/UserController.java
+++ src/main/java/Hungry/PatientAnalystic/controller/UserController.java
@@ -1,2 +1,36 @@
-package Hungry.PatientAnalystic.controller;public class UserController {
+package Hungry.PatientAnalystic.controller;
+
+import Hungry.PatientAnalystic.dto.UserAccountDto;
+import Hungry.PatientAnalystic.repository.UserAccountRepository;
+import Hungry.PatientAnalystic.service.UserAccountService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("user")
+public class UserController {
+
+    private final UserAccountRepository userAccountRepository;
+    private final UserAccountService userAccountService;
+    //create user
+    @PostMapping ("/create")
+    public void createUser(UserAccountDto userAccountDto){
+        userAccountRepository.save(userAccountDto.toEntity());
+    }
+
+    @GetMapping("/search")
+    public void searchUser(@RequestParam String username){
+        userAccountService.searchUser(username);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteUser(@RequestParam String username){
+        userAccountRepository.deleteById(username);
+    }
+
+    @PutMapping("/update")
+    public void updateUser(UserAccountDto userAccountDto){
+        userAccountRepository.save(userAccountDto.toEntity());
+    }
 }
