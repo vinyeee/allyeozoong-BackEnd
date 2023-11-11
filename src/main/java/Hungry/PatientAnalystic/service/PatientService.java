e3df41fab1c478f280484c5ea3a0371e47d336d0
yooonwodyd <dbswodyd00@naver.com>
2023-11-12 01:26:22 +0900

    PatientService 재변경

--- src/main/java/Hungry/PatientAnalystic/service/PatientService.java
+++ src/main/java/Hungry/PatientAnalystic/service/PatientService.java
@@ -5,6 +5,7 @@ import Hungry.PatientAnalystic.dto.GroupAges;
 import Hungry.PatientAnalystic.dto.PatientDto;
 import Hungry.PatientAnalystic.domain.Patient;
 import Hungry.PatientAnalystic.repository.PatientRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,66 +19,31 @@ public class PatientService {
     private final PatientRepository patientRepository;
 
 
-    public void create(PatientDto dto){
-
-        Patient patient = new Patient();
-
-        int age = (dto.getAge() / 10 ) * 10; // user 실제 나이에서 일의 자리 제거 ex) 23 => 20대로 만들어줌
-        patient.setName(dto.getName());
-        patient.setSymptom(dto.getSymptom());
-        patient.setAge(age);
-        patient.setLisk(dto.getLisk());
-
+    @Transactional
+    public void create(Patient patient){
         patientRepository.save(patient);
     }
 
+    @Transactional
     public double getFrequency(String symptom){
         Frequency frequencyDTO = patientRepository.findFrequencyBySymptom(symptom); // 증상 + count
         long count = frequencyDTO.getCount(); // count
-
         long total = patientRepository.getCount();
-
         double percentage =   ((double) count / total ) * 100;
-
-        // Math.round를 사용하여 소수점 두 자리까지 반올림
         percentage = (double) Math.round(percentage * 100) / 100;
-
-
-//        System.out.println(total);
-//        System.out.println(count);
-//        System.out.println(percentage);
-//        String frequency = "";
-
-//        // 개체군 100명이라 가정
-//        if(count >= 50) {
-//            frequency = "자주 보임";
-//        }else if(count >= 30){
-//            frequency = "가끔 보임";
-//        }else if(count >= 10){
-//            frequency = "드물게 보임";
-//        }else{
-//            frequency = "거의 보이지 않음";
-//        }
-
-
         return percentage;
     }
 
 
+    @Transactional
     public List<GroupAges> getCountOfAgeGroup(String symptom){
 
         List<GroupAges> groupAges = patientRepository.findSymptomAges(symptom);
-
-        //Map<Integer, Long> ageStatistics = new HashMap<>();
-//        for (GroupAges groupAge : groupAges) {
-//            ageStatistics.put(groupAge.getGroup(), groupAge.getCount());
-//        }
-
         return groupAges;
     }
 
     public List<PatientDto> findAllByName(String name){
-        return patientRepository.findAllByName(name).stream().map(PatientDto::fromEntity).collect(Collectors.toList());
+        return patientRepository.findAllByName(name).stream().map(PatientDto::from).collect(Collectors.toList());
     }
 
 
